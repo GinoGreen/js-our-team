42da5b94ef61e747076e950c9b1cbef9be961220
GinoGreen <luigiverde97@gmail.com>
2021-11-04 17:41:38 +0100

    Esercitazione completata no bonus

--- js/script.js
+++ js/script.js
@@ -3,49 +3,40 @@
    2. inizializzo un oggetto che contiene le info del membro
 */
 let teamMembers = [];
+
 const member1 = {
-   name : '1',
-   role : '1',
-   photo : 'img\angela-caroll-chief-editor.jpg'
+   name : 'Angela Caroll',
+   role : 'Chief Editor',
+   photo : 'img/angela-caroll-chief-editor.jpg'
 };
 const member2 = {
-   name : '2',
-   role : '2',
-   photo : 'img\angela-lopez-social-media-manager.jpg'
+   name : 'Angela Lopez',
+   role : 'Social Media Manager',
+   photo : 'img/angela-lopez-social-media-manager.jpg'
 };
 const member3 = {
-   name : '3',
-   role : '3',
-   photo : 'img\barbara-ramos-graphic-designer.jpg'
+   name : 'Barbara Ramos',
+   role : 'Graphic Deisgner',
+   photo : 'img/barbara-ramos-graphic-designer.jpg'
 };
 const member4 = {
-   name : '4',
-   role : '4',
-   photo : 'img\scott-estrada-developer.jpg'
+   name : 'Scott Estrada',
+   role : 'Developer',
+   photo : 'img/scott-estrada-developer.jpg'
 };
 const member5 = {
-   name : '5',
-   role : '5',
-   photo : 'img\walter-gordon-office-manager.jpg'
+   name : 'Walter Gordon',
+   role : 'Office Manager',
+   photo : 'img/walter-gordon-office-manager.jpg'
 };
-
-const teamCard = document.createElement('div');
-const cardImage = document.createElement('div');
-const cardText = document.createElement('div');
-const nameText = document.createElement('h3');
-const roleText = document.createElement('p');
-
-teamCard.className = 'team-card';
-cardImage.className = 'card-image';
-cardText.className = 'card-text';
 
 //pusho all'interno del vettore tutti i membri
 teamMembers = pushMember();
 
 //
-console.log('log 1', teamMembers);
-printMemberTeam();
+console.log('log array', teamMembers);
 
+generateMembersTeamDOM();
 
 /* - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.*/
 
@@ -59,39 +50,55 @@ function pushMember () {
    teamMembers.push(member4);
    teamMembers.push(member5);
    
-   console.log(teamMembers);
-   
    return teamMembers;
 }
 
-function printMemberTeam () {
+function generateMembersTeamDOM () {
 
-   const teamContainer = document.querySelector('.team-container');
+   for (let i = 0; i < teamMembers.length; i++) {
+      
+      const teamContainer = document.querySelector('.team-container');
 
-   for (let index of teamMembers) {
-
-      const member = teamMembers[index];
-
-      console.log(member.name);
-      console.log(member.role);
-      console.log(member.photo);
+      const member = getMember(i);
       
-      cardImage.innerHTML = `<img src="${member.photo}" alt=""/>`;
-      
-      nameText.innerHTML = member.name;
-      
-      roleText.innerHTML = member.role;
-      
-      cardText.innerHTML = nameText;
-      
-      cardText.innerHTML += roleText;
-      
-      teamCard.innerHTML = cardImage;
-      
-      teamCard.innerHTML += cardText;
-
-      teamContainer.innerHTML += teamCard;
-
+      teamContainer.append(member);
    }
 
+}
+
+function getMember (index) {
+
+   console.log('index', index);
+   console.log('teamMembers[index]', teamMembers[index]);
+
+   const teamCard = document.createElement('div');
+   const cardImage = document.createElement('div');
+   const cardText = document.createElement('div');
+
+   teamCard.className = 'team-card';
+   cardImage.className = 'card-image';
+   cardText.className = 'card-text';
+
+   const member = teamMembers[index];
+
+   /*console.log(member.name);
+   console.log(member.role);
+   console.log(member.photo);*/
+   
+   cardImage.innerHTML = `<img src="${member.photo}" alt=""/>`;
+   
+   cardText.innerHTML = `
+      <h3>${member.name}</h3>
+      <p>${member.role}</p>
+   `;
+
+   teamCard.innerHTML = '';
+   
+   teamCard.append(cardImage);
+   
+   teamCard.append(cardText);
+
+   console.log('teamCard', teamCard)
+
+   return teamCard;
 }
